4d34262e48e2eb724938c685d28c721e41309136
priasetia6282-beep <priasetia6282@gmail.com>
2025-10-02 12:13:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-
-function ubahNama() {
-      let input = document.getElementById("inputNama").value;
-      if (input.trim() !== "") {
-        document.getElementById("nama").textContent = input;
-        document.getElementById("inputNama").value = "";
-      } else {
-        alert("Isi dulu nama barunya!");
-      }
-    }
-
-    function sapa() {
-      let nama = document.getElementById("nama").textContent;
-      alert("Halo, " + nama + " 👋");
-    }
+const tombolSapa = document.getElementById('sapabutton');
+tombolSapa.addEventListener('click', function(){
+    alert('Halo! thx ws berkunjung')
+});
