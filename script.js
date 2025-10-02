e96fae8d753f72a8325feffb0dfac5474feae074
priasetia6282-beep <priasetia6282@gmail.com>
2025-10-02 11:54:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function ubahNama() {
+      let input = document.getElementById("inputNama").value;
+      if (input.trim() !== "") {
+        document.getElementById("nama").textContent = input;
+        document.getElementById("inputNama").value = "";
+      } else {
+        alert("Isi dulu nama barunya!");
+      }
+    }
+
+    function sapa() {
+      let nama = document.getElementById("nama").textContent;
+      alert("Halo, " + nama + " 👋");
+    }
